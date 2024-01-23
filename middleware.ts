d1d7b73898b87e5b0b70843086a91ef5102672b4
hjk <these990712@gmail.com>
2024-01-24 00:33:46 +0900

    update

--- middleware.ts
+++ middleware.ts
@@ -5,17 +5,7 @@ import {geolocation} from "@vercel/edge";
 const middleware = (request: NextRequest, event: NextFetchEvent) => {
     const cookies = request.cookies
     const requestHeaders = new Headers(request.headers)
-
-    const response = NextResponse.next({
-        request: {
-            headers: requestHeaders
-        }
-    })
-    setHeader(response, 'str', 'str')
-
     const {ip} = request
-    console.log(request)
-    return response
 }
 
 const setHeader = (response: NextResponse, key: string, value: any) => {
